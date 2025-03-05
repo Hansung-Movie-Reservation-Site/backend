ffb4219713ebd14d1b2e20c9614fe3a7fa1a59e4
whereful <where5224@naver.com>
2025-03-05 14:26:41 +0900

    Room 엔티티 수정 및 repository 수정

--- src/main/java/com/springstudy/backend/API/Repository/RoomRepository.java
+++ src/main/java/com/springstudy/backend/API/Repository/RoomRepository.java
@@ -1,12 +1,14 @@
 package com.springstudy.backend.API.Repository;
 
 import com.springstudy.backend.API.Repository.Entity.Room;
-import com.springstudy.backend.API.Repository.Entity.RoomId;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 
 @Repository
-public interface RoomRepository extends JpaRepository<Room, RoomId> {
+public interface RoomRepository extends JpaRepository<Room, Long> {
 
+    // ✅ Spot ID로 Room 목록 조회
+    List<Room> findBySpotId(Long spotId);
 }
