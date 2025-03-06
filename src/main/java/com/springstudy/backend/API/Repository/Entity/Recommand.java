1d4f3fd0b666e65e55b6b13cc7fe9fe35297e4db
ghkdwnsdlr <imbanana15@gmail.com>
2025-03-07 01:14:46 +0900

    영화 추천 성공

---
+++ src/main/java/com/springstudy/backend/API/Repository/Entity/Recommand.java
@@ -0,0 +1,26 @@
+package com.springstudy.backend.API.Repository.Entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Getter
+@Builder
+public class Recommand {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String movie_name;
+
+    @Column(nullable = false)
+    private Long recommand_movie_id;
+    // movie와 OneToOne 할까 생각했으나
+    // 복잡해지고 성능 생각해서 id만 저장하고 필요할 때 검색될 때 id로 그때그때 조회하는 걸로.
+
+    //todo
+    //예매내역 엔티티와 연결할 것.
+}
