69f877df129fe8e49f23f427882819d78ce49599
whereful <where5224@naver.com>
2025-03-11 17:27:35 +0900

    Ticket 엔티티 형식 맞추기, order 외래키 추가

--- src/main/java/com/springstudy/backend/API/Repository/Entity/Ticket.java
+++ src/main/java/com/springstudy/backend/API/Repository/Entity/Ticket.java
@@ -1,22 +1,26 @@
 package com.springstudy.backend.API.Repository.Entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-
-import java.time.LocalDate;
+import lombok.*;
 
 @Entity
 @Table(
         name = "ticket",
         uniqueConstraints = @UniqueConstraint(
                 name = "unique_ticket_constraint",
-                columnNames = { "screeningid", "userid", "horizontal", "vertical", "purchase_time", "price"}
+                columnNames = {
+                        "screeningid",
+                        "seatid",
+                        "userid",
+                        "horizontal",
+                        "vertical",
+                        "price",
+                        "userid",
+                        "ordersid"}
         )
 )
 @Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
@@ -25,11 +29,6 @@ public class Ticket {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    @ManyToOne
-    @JoinColumn(name = "userid", referencedColumnName = "id",
-            foreignKey = @ForeignKey(name = "FK_ticket_TO_user"))
-    private User user; // ✅ 구매한 사용자 ID를 외래키로 참조
 
     @ManyToOne
     @JoinColumn(name = "screeningid", referencedColumnName = "id", nullable = false,
@@ -41,18 +40,23 @@ public class Ticket {
             foreignKey = @ForeignKey(name = "FK_ticket_TO_seat_1"))
     private Seat seat;
 
-    @Column(nullable = false)
-    private LocalDate purchase_time;  // ✅ 구매 시간
-
-    @Column(nullable = false)
-    private int price;
-
     @Column(nullable = false, length = 1)
     private String horizontal;
 
     @Column(nullable = false)
     private int vertical;
 
+    @Column(nullable = false)
+    private int price;
 
+    @ManyToOne
+    @JoinColumn(name = "userid", referencedColumnName = "id",
+            foreignKey = @ForeignKey(name = "FK_ticket_TO_user"))
+    private User user; // ✅ 구매한 사용자 ID를 외래키로 참조
+
+    @ManyToOne
+    @JoinColumn(name = "ordersid", referencedColumnName = "id",
+            foreignKey = @ForeignKey(name = "FK_ticket_TO_orders"))
+    private Order order;
 
 }
