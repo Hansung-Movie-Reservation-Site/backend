42e3de44ae4ef20c82a896868829ee2bcb0fd980
ghkdwnsdlr <imbanana15@gmail.com>
2025-04-04 21:44:05 +0900

    추천기능 수정1

--- src/main/java/com/springstudy/backend/API/AI/Controller/AIcontroller.java
+++ src/main/java/com/springstudy/backend/API/AI/Controller/AIcontroller.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("AIRecommand")
+@RequestMapping("/api/v1/AIRecommand")
 @RequiredArgsConstructor
 public class AIcontroller {
     private final AIService aiService;
