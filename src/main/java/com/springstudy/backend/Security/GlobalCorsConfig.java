6ac252e1e1c8e5731d4230a948a1cc3a538e6ab7
whereful <where5224@naver.com>
2025-05-29 17:53:09 +0900

    [테스트] WebMvcConfigurer 추가 -> 실패

--- src/main/java/com/springstudy/backend/Security/GlobalCorsConfig.java
+++ src/main/java/com/springstudy/backend/Security/GlobalCorsConfig.java
@@ -1,17 +1,17 @@
-package com.springstudy.backend.Security;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Configuration
-public class GlobalCorsConfig implements WebMvcConfigurer {
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-            .allowedOrigins("https://cinemagix-xi.vercel.app")
-            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-            .allowedHeaders("*")
-            .allowCredentials(true);
-    }
-}
+//package com.springstudy.backend.Security;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//@Configuration
+//public class GlobalCorsConfig implements WebMvcConfigurer {
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")
+//            .allowedOrigins("https://cinemagix-xi.vercel.app")
+//            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+//            .allowedHeaders("*")
+//            .allowCredentials(true);
+//    }
+//}
